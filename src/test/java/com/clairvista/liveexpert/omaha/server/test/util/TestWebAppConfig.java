84166774bd0328e4aff3567093c9d61f52230c10
Harrison Strowd <hstrowd@clairvista.com>
2013-07-23 10:17:26 -0500

    Removes a lot of the spew from the logs.

--- src/test/java/com/clairvista/liveexpert/omaha/server/test/util/TestWebAppConfig.java
+++ src/test/java/com/clairvista/liveexpert/omaha/server/test/util/TestWebAppConfig.java
@@ -39,9 +39,9 @@ public class TestWebAppConfig {
    private static final String PROPERTY_NAME_HIBERNATE_DIALECT = "hibernate.dialect";
    private static final String HIBERNATE_DIALECT = "org.hibernate.dialect.HSQLDialect";
    private static final String PROPERTY_NAME_HIBERNATE_SHOW_SQL = "hibernate.show_sql";
-   private static final String HIBERNATE_SHOW_SQL = "true";
+   private static final String HIBERNATE_SHOW_SQL = "false";
    private static final String PROPERTY_NAME_HIBERNATE_AUTO_ACTION = "hibernate.hbm2ddl.auto";
-   private static final String HIBERNATE_AUTO_ACTION = "create";
+   private static final String HIBERNATE_AUTO_ACTION = "create-drop";
    private static final String PROPERTY_NAME_ENTITYMANAGER_PACKAGES_TO_SCAN = "entitymanager.packages.to.scan";
     
    @Resource
