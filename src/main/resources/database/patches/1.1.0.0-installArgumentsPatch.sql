5fef5a3a7b2f2abef1bd039cb726a1d19cd0cb90
Harrison Strowd <hstrowd@clairvista.com>
2014-01-16 20:38:38 -0600

    BugzID:2973 -- Corrects the install arguments DB patch.

--- src/main/resources/database/patches/1.1.0.0-installArgumentsPatch.sql
+++ src/main/resources/database/patches/1.1.0.0-installArgumentsPatch.sql
@@ -5,13 +5,13 @@ DROP PROCEDURE IF EXISTS leomaha.add_install_arguments;
 delimiter //
 CREATE PROCEDURE leomaha.add_install_arguments() BEGIN
    
-   IF EXISTS (SELECT 1
-                FROM information_schema.columns
-               WHERE table_name = 'applications'
-                 AND column_name  = 'install_arguments'
-                 AND table_schema = 'leomaha') THEN
+   IF NOT EXISTS (SELECT 1
+                    FROM information_schema.columns
+                   WHERE table_name = 'applications'
+                     AND column_name  = 'install_arguments'
+                     AND table_schema = 'leomaha') THEN
 
-       ALTER TABLE liveexpert.applications
+       ALTER TABLE leomaha.applications
               ADD COLUMN install_arguments VARCHAR(256) AFTER app_id;
       
    ELSE 
